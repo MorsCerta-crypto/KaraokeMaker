068a62b568bb66e2f405479bba599fdceb26ac0e
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-02-28 12:36:26 +0100

    find lyrics and store them

--- karaoke-maker/src/lyrics/lyrics.py
+++ karaoke-maker/src/lyrics/lyrics.py
@@ -1,9 +1,15 @@
+import os
+from pathlib import Path
 from typing import Optional
 from lyricsgenius import Genius
 
 
 class SongLyrics:
     def __init__(self):
+        self.lyrics_str=""
+        self.export_dir = Path("/karaoke-maker/data/lyrics/")
+        #if not os.path.exists(self.export_dir):
+        #    os.makedirs(self.export_dir)
         self.genius = Genius()
         # Turn off status messages
         self.genius.verbose = False
@@ -21,6 +27,8 @@ class SongLyrics:
         """find the song by its title"""
         song = self.genius.search_song(title=song_title)
         if song:
+            self.song_name = song.full_title.encode("ascii", "ignore").decode()
+            self.lyrics_str = song.to_text()
             return song.lyrics
 
     def get_song_name_by_lyrics(self, lyrics_term: str) -> list[str]:
@@ -31,11 +39,25 @@ class SongLyrics:
             song_names.append(hit["result"]["title"])
         return song_names
 
-    def get_lyrics_by_artist_and_song(self, artist_name: str, song: str) -> Optional[str]:
+    def get_lyrics_by_artist_and_song(self, artist_name: str, song_title: str) -> Optional[str]:
         """find the lyrics for a song with a known artist"""
         # artist = self.genius.search_artist(artist_name, max_songs=1)
 
-        search_song = self.genius.search_song(title=song, artist=artist_name)
-        if search_song:
-            return str(search_song.lyrics)
+        song = self.genius.search_song(title=song_title, artist=artist_name)
+        if song:
+            self.song_name = song.full_title.encode("ascii", "ignore").decode()
+            self.lyrics_str =  song.to_text()
+            return song.to_text()
         
+    def on_success(self):
+        name = self.song_name+".txt"
+        path = self.export_dir/name
+        if self.lyrics_str == "" or self.lyrics_str is None:
+            raise ValueError("lyrics are empty")
+        #if not os.path.exists(path):
+        #    os.makedirs(path)
+        with open(path,"w+") as f:
+            f.write(self.lyrics_str)
+            
+        
+
