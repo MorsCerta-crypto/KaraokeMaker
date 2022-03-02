b5a62fb570f4fac599f97b69a69fd485fc05aeaa
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-02 13:48:24 +0100

    make lyric window a subwindow

--- karaoke-maker/src/gui/lyrics_window.py
+++ karaoke-maker/src/gui/lyrics_window.py
@@ -1,20 +1,21 @@
-from tkinter import Tk,StringVar,N,Label,mainloop,CENTER
+from tkinter import Tk,StringVar,N,Label,mainloop,CENTER,Toplevel
 
-def show_lyrics(lyrics): 
+def show_lyrics(lyrics,root): 
 
-    master = Tk() 
+    master = Toplevel(root) 
     master.title("Lyrics")
-    master.geometry("600x1000+100+100")
+    master.geometry("800x1000+100+100")
     master.configure(bg='light grey') 
 
     result = StringVar()
     result.set(lyrics)
     
     Label(master, text="", textvariable=result, 
-        bg="light grey").grid(sticky=N) 
+        bg="light grey",font=("times new roman",15,"bold")).grid(sticky=N) 
 
     mainloop()
            
 if __name__ == "__main__":
-    show_lyrics("Songname\n\nhere are the \nlyrics")
+    root = Tk()
+    show_lyrics("Songname\n\nhere are the \nlyrics",root)
 
