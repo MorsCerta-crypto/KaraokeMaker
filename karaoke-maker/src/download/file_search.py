61ad74168250d09b35abe2cb6fe9ef81350895ff
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-02 10:36:56 +0100

    song object instead of name and path

--- karaoke-maker/src/download/file_search.py
+++ karaoke-maker/src/download/file_search.py
@@ -6,22 +6,19 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional, Union
 import pickle
+from ..song import Song
 
 
 @dataclass
 class DownloadedSongs:
     def __init__(self, songs_path: str = "/data/songs/"):
         self.songs_path = songs_path + "downloads.txt"
-        self.song_name = ""
-        self.song_path = ""
+        self.song = None
 
-    def handle_download_success(self, song_name: Optional[str]):
+    def handle_download_success(self, song: Song):
         """stores an song-path-pair if download was successfully"""
-        if song_name:
-            self.song_name = song_name
-        if self.song_name != "" and self.song_path != "":
-            print("attr:", self.song_name, self.song_path)
-            self.add_songs_to_file()
+        if song is not None:
+            self.add_songs_to_file(song)
         else:
             raise ValueError("song_name and song_path must not be empty")
 
@@ -32,37 +29,22 @@ class DownloadedSongs:
         if not downloaded_songs:
             # no downloads
             return False
-        if path in downloaded_songs.values():
-            ind = list(downloaded_songs.values()).index(path)
-            self.song_name = list(downloaded_songs.keys())[ind]
-            return True
+        for song in downloaded_songs:
+            if path == song.file_path:
+                self.song = song
+                return True
         return False
 
-    def search_song(self, song_name: str) -> Optional[str]:
-        """searches song name first in directory then on spotify
-
-        Args:
-            song_name (str): name for a song, spelling is important
-
-        Returns:
-            str: path where song is / will be
-        """
-        self.song_name = song_name  # store for later
-        maybe_path = self.song_path_from_name()  # check if song is in downloads
-        if maybe_path:
-            return maybe_path
-
-    def song_path_from_name(self) -> Union[str, None]:
+    def song_path_from_name(self,name) -> Optional[str]:
         """returns a path if song name was found in file"""
         songs = self.read_songs_from_file()
         if not songs:
             return None
-        if self.song_name in songs.keys():
-            return songs[self.song_name]
-        else:
-            return None
+        for song in songs:
+            if name in song.song_name:
+                return song.file_path
 
-    def read_songs_from_file(self) -> Union[dict, None]:
+    def read_songs_from_file(self) -> Optional[list]:
         # saving_path = Path(self.songs_path)
         # saving_path.parent.mkdir(parents=True, exist_ok=True)
 
@@ -70,18 +52,17 @@ class DownloadedSongs:
             songs = pickle.load(fp)
         return songs
 
-    def add_songs_to_file(self) -> None:
+    def add_songs_to_file(self,song) -> None:
         """if a song was searched add it to a temp file, if downloading was a success, add it to the song list
 
         Args:
             song_name (str): name of the currently searched song
             song_path (str): path where the download will go
         """
-        # current_songs = self.read_songs_from_file()
-        current_songs = dict()
-        if not current_songs:
-            current_songs = dict()
-        current_songs[self.song_name] = self.song_path
+        current_songs = self.read_songs_from_file()
+        if current_songs:
+            current_songs.append(song)
+        else: current_songs = [song]
 
         with open(self.songs_path, "wb") as fp:
             pickle.dump(current_songs, fp)
