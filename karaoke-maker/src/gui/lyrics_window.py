620b59ff485fb62ce34aa0a5a00e8f8082b966dd
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-03 13:38:42 +0100

    visual improvements

--- karaoke-maker/src/gui/lyrics_window.py
+++ karaoke-maker/src/gui/lyrics_window.py
@@ -1,17 +1,17 @@
-from tkinter import Tk,StringVar,N,Label,mainloop,CENTER,Toplevel
+from tkinter import Tk,StringVar,N,Label,mainloop,CENTER,Toplevel,VERTICAL
 
 def show_lyrics(lyrics,root): 
 
-    master = Toplevel(root) 
+    master = root#Toplevel(root) 
     master.title("Lyrics")
-    master.geometry("800x1000+100+100")
+    master.geometry("700x1000") # left top
     master.configure(bg='light grey') 
 
     result = StringVar()
     result.set(lyrics)
     
     Label(master, text="", textvariable=result, 
-        bg="light grey",font=("times new roman",15,"bold")).grid(sticky=N) 
+        bg="light grey",font=("arial",14,"bold")).grid(sticky=N,padx=20,pady=20) 
 
     mainloop()
            
