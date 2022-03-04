cfe4a95a4b14e99a5661a21453233ddc2b1a0f89
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-04 12:54:35 +0100

    path type

--- karaoke-maker/src/download/file_search.py
+++ karaoke-maker/src/download/file_search.py
@@ -13,7 +13,7 @@ from ..song import Song
 @dataclass
 class DownloadedSongs:
     def __init__(self, songs_path: str = "karaoke-maker/data/downloads/"):
-        self.songs_path = songs_path + "downloads.txt"
+        self.songs_path = Path(songs_path) / "downloads.txt"
         self.song = None
 
     def handle_download_success(self, song: Song):
@@ -46,8 +46,8 @@ class DownloadedSongs:
                 return song.file_path
 
     def read_songs_from_file(self) -> Optional[list]:
-        saving_path = Path(self.songs_path)
-        saving_path.parent.mkdir(parents=True, exist_ok=True)
+        
+        self.songs_path.parent.mkdir(parents=True, exist_ok=True)
         
         if os.path.getsize(self.songs_path) > 0:     
             with open(self.songs_path,"rb") as f:
@@ -55,6 +55,13 @@ class DownloadedSongs:
                 songs = unpickler.load()
                 return songs
 
+    def songs_in_folder(self)->list[str]:
+        songs = []
+        path = self.songs_path.parent
+        for file in path.iterdir():
+            if file.suffix in [".mp3",".wav",".ogg"]:
+                songs.append(str(file))
+        return songs
         
 
     def add_songs_to_file(self,song) -> None:
@@ -64,10 +71,16 @@ class DownloadedSongs:
             song (Song): obj of the currently searched song
             
         """
+        
+        available_songs = self.songs_in_folder()
         current_songs = self.read_songs_from_file()
+        len_start = len(current_songs)
         if current_songs:
             current_songs.append(song)
         else: current_songs = [song]
-
+        for index,song in enumerate(current_songs):
+            if song.file_path in available_songs:
+                current_songs.remove(index)
+        print("deleted :", len(current_songs)-len_start)
         with open(self.songs_path, "wb") as fp:
             pickle.dump(current_songs, fp)
