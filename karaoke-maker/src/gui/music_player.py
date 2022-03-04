c87816901dc97a9305a49d99580a3952a3da5ae4
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-04 12:55:42 +0100

    music player improvements

--- karaoke-maker/src/gui/music_player.py
+++ karaoke-maker/src/gui/music_player.py
@@ -2,32 +2,33 @@ from tkinter import (StringVar,Toplevel,LabelFrame,Label,
                      Button,Scrollbar,Listbox,VERTICAL,RIGHT,
                      Tk,BOTH,END,ACTIVE,SINGLE,GROOVE,Y)
 import pygame
+from pydub import AudioSegment
 
-class MusicPlayer(object):
+class MusicPlayer(Toplevel):
     
     def __init__(self,root):
-        self.root = Toplevel(root)
-        self.root.title("MusicPlayer")
-        self.root.geometry("950x250+700+0")
-        self.root.protocol("WM_DELETE_WINDOW", self.on_closing)
+        super().__init__(root)
+        self.title("MusicPlayer")
+        self.geometry("950x250+710+10")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
         pygame.init()
         pygame.mixer.init()
         self.track = StringVar()
         self.status = StringVar()
 
-        trackframe = LabelFrame(self.root,text="Song Track",font=("times new roman",15,"bold"),bg="Navyblue",fg="white",bd=5,relief=GROOVE)
+        trackframe = LabelFrame(self,text="Song Track",font=("times new roman",15,"bold"),bg="Navyblue",fg="white",bd=5,relief=GROOVE)
         trackframe.place(x=0,y=0,width=600,height=100)
         Label(trackframe,textvariable=self.track,width=40,font=("times new roman",18,"bold"),bg="Orange",fg="gold").grid(row=0,column=0,padx=10,pady=5)
         Label(trackframe,textvariable=self.status,font=("times new roman",15,"bold"),bg="orange",fg="gold").grid(row=0,column=1,padx=10,pady=5)
 
-        buttonframe = LabelFrame(self.root,text="Control Panel",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
+        buttonframe = LabelFrame(self,text="Control Panel",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
         buttonframe.place(x=0,y=100,width=600,height=100)
         Button(buttonframe,text="PLAYSONG",command=self.playsong,width=11,height=1,font=("times new roman",15,"bold"),fg="navyblue",bg="pink").grid(row=0,column=0,padx=10,pady=5)
         Button(buttonframe,text="PAUSE",command=self.pausesong,width=9,height=1,font=("times new roman",15,"bold"),fg="navyblue",bg="pink").grid(row=0,column=1,padx=10,pady=5)
         Button(buttonframe,text="UNPAUSE",command=self.unpausesong,width=11,height=1,font=("arial",15,"bold"),fg="navyblue",bg="pink").grid(row=0,column=2,padx=10,pady=5)
         Button(buttonframe,text="STOPSONG",command=self.stopsong,width=11,height=1,font=("arial",15,"bold"),fg="navyblue",bg="pink").grid(row=0,column=3,padx=10,pady=5)
         Button(buttonframe,text="NEXT SONG",command=self.nextsong,width=11,height=1,font=("arial",15,"bold"),fg="navyblue",bg="pink").grid(row=0,column=4,padx=10,pady=5)
-        songsframe = LabelFrame(self.root,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
+        songsframe = LabelFrame(self,text="Song Playlist",font=("times new roman",15,"bold"),bg="grey",fg="white",bd=5,relief=GROOVE)
         songsframe.place(x=600,y=0,width=400,height=200)
         scrol_y = Scrollbar(songsframe,orient=VERTICAL)
         self.playlist = Listbox(songsframe,yscrollcommand=scrol_y.set,selectbackground="gold",selectmode=SINGLE,font=("times new roman",12,"bold"),bg="silver",fg="navyblue",bd=5,relief=GROOVE)
@@ -47,12 +48,15 @@ class MusicPlayer(object):
         self.playlist.insert(END,track)
 
     def playsong(self):
-        
-        if self.playlist.get(ACTIVE) is None:
+        active_song = self.playlist.get(ACTIVE)
+        if not active_song:
             raise ValueError("no song given")
-        self.track.set(self.playlist.get(ACTIVE))
+        if active_song.endswith(".mp3"):
+            print("mp3 could be skipped")
+            #self.nextsong()
+        self.track.set(active_song)
         self.status.set("-Playing")
-        pygame.mixer.music.load(self.playlist.get(ACTIVE))
+        pygame.mixer.music.load(active_song)
         pygame.mixer.music.play()
         
     def stopsong(self):
@@ -69,7 +73,7 @@ class MusicPlayer(object):
         
     def on_closing(self):
         self.stopsong()
-        self.root.destroy()
+        #self.destroy()
 
 
         
