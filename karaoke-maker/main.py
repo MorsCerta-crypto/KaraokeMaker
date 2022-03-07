e850bf0ed4d5c02132b40843853cf0be5a70de15
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-07 12:01:37 +0100

    better file structure

--- karaoke-maker/main.py
+++ karaoke-maker/main.py
@@ -1,5 +1,5 @@
 
-from src.gui.interface import run_gui
+from gui.interface import run_gui
 import json
 
 
@@ -11,28 +11,6 @@ def read_config():
 def run_project():
     """runs project via interface or with normal commands"""
     config = read_config()
-    # term = input("enter song name\n")
-    # search = Search(
-    #     output_format=CONFIG["song_format"], songs_path=CONFIG["output_path"]
-    # )
-    # song = search.from_search_term(term)
-    # if song is None:
-    #     raise ValueError("song was not found")
-    # print("song was found...")
-
-    # downloader = Downloader(
-    #     format=CONFIG["song_format"],
-    #     output_path=CONFIG["output_path"],
-    #     ytdl_format=CONFIG["ytdl_format"],
-    # )
-    # path = downloader.download_song(song)
-    # if path is None:
-    #     raise ValueError("song could not be downloaded/found")
-    # print("song was downloaded to ", path)
-
-    # remover = VocalRemover()
-    # remover.remove_vocals(path)
-    
     run_gui(config)
     
 
