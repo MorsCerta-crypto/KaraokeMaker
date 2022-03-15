2510d9edb3caedd93cbdaba7b386404175a047b8
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-15 13:56:25 +0100

    update imports

--- karaoke-maker/backend/__init__.py
+++ karaoke-maker/backend/__init__.py
@@ -1,8 +1,7 @@
 
 from .downloader import Downloader
 from .lyrics import SongLyrics
-from .song import Song
 from .search import Search, DownloadedSongs, yt_search
 from .vocalremover import VocalRemover
 
-__all__ = ['Song','SongLyrics','Downloader','Search','DownloadedSongs','VocalRemover','yt_search']
+__all__ = ['SongLyrics','Downloader','Search','DownloadedSongs','VocalRemover','yt_search']
