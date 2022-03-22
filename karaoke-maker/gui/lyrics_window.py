ca0c1d869769e1106e8c9b3f3ebb53d175b50b0e
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-22 21:45:06 +0100

    move to class and ttk

--- karaoke-maker/gui/lyrics_window.py
+++ karaoke-maker/gui/lyrics_window.py
@@ -1,21 +1,39 @@
-from tkinter import Tk,StringVar,N,Label,mainloop
+import tkinter as tk# Tk,StringVar,N,Label,mainloop
+from tkinter import ttk
 
-def show_lyrics(lyrics,root): 
-    """creates a window to show the lyrics(str)"""
-    master = root
-    master.title("Lyrics")
-    master.geometry("700x1000") # left top
-    master.configure(bg='light grey') 
+BAD_WORDS = ["embed","lyrics"]
 
-    result = StringVar()
-    result.set(lyrics)
-    
-    Label(master, text="", textvariable=result, 
-        bg="light grey",font=("arial",14,"bold")).grid(sticky=N,padx=20,pady=20) 
+class LyricsWindow(ttk.Frame):
+    def __init__(self,root):
+        super().__init__(root)
+        
+        lyrics_frame = ttk.Frame(self)
+        header = ttk.Label(lyrics_frame, text="Lyrics",font=("arial",22))
+        header.grid(row=0,column=0)
+        self.Lyrics = ttk.Label(lyrics_frame, text="", 
+                                font=("arial",16))
+        self.Lyrics.grid(row=1,rowspan=2, column=0,sticky=tk.NSEW,padx=20,pady=20,ipady=20,ipadx=20) 
+        
+        lyrics_frame.grid(sticky=tk.NSEW,rowspan=3)
+        lyrics_frame.grid_rowconfigure(index=1,weight=1,minsize=50)
+        
+        
+    def set_lyrics(self,text):
+        """changes text of lyrics window"""
+        
+        value = self._preprocess_text(text)
+        if self.Lyrics is not None:
+            self.Lyrics["text"] = value
+        
 
-    mainloop()
-           
-if __name__ == "__main__":
-    root = Tk()
-    show_lyrics("Songname\n\nhere are the \nlyrics",root)
+    def _preprocess_text(self,text):
+        """removes unwanted words from text"""
+        
+        for word in BAD_WORDS:
+            if word in text.lower():
+                text = text.replace(word,"\n")
+        return text
+        
+        
+
 
