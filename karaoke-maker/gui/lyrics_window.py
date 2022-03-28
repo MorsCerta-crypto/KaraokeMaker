ba7346152efb7c478106c65d9cae75bb5074d151
Konrad <62557233+MorsCerta-crypto@users.noreply.github.com>
2022-03-28 13:52:36 +0200

    remove "Lyrics" and "embed", style change

--- karaoke-maker/gui/lyrics_window.py
+++ karaoke-maker/gui/lyrics_window.py
@@ -1,3 +1,4 @@
+from tarfile import _Bz2ReadableFileobj
 import tkinter as tk# Tk,StringVar,N,Label,mainloop
 from tkinter import ttk
 
@@ -10,29 +11,68 @@ class LyricsWindow(ttk.Frame):
         lyrics_frame = ttk.Frame(self)
         header = ttk.Label(lyrics_frame, text="Lyrics",font=("arial",22))
         header.grid(row=0,column=0)
-        self.Lyrics = ttk.Label(lyrics_frame, text="", 
-                                font=("arial",16))
-        self.Lyrics.grid(row=1,rowspan=2, column=0,sticky=tk.NSEW,padx=20,pady=20,ipady=20,ipadx=20) 
+        scrol_y = tk.Scrollbar(lyrics_frame,orient=tk.VERTICAL,relief=tk.GROOVE)
+        self.Lyrics = tk.Listbox(lyrics_frame, 
+                                font=("courier 11",16),
+                                yscrollcommand=scrol_y.set,
+                                selectmode=tk.NONE)
         
-        lyrics_frame.grid(sticky=tk.NSEW,rowspan=3)
+        self.Lyrics.grid(row=1,
+                         rowspan=2,
+                         column=0,
+                         sticky=tk.NSEW,
+                         padx=5, 
+                         pady=5,
+                         ipady=20,
+                         ipadx=20) 
+    
+        scrol_y.grid(row=1,rowspan=2,column=1,sticky=tk.NSEW)
+        scrol_y.config(command=self.Lyrics.yview)
+        
+        lyrics_frame.grid(column=0,row=0,sticky=tk.NSEW,rowspan=3)
         lyrics_frame.grid_rowconfigure(index=1,weight=1,minsize=50)
+        lyrics_frame.grid_columnconfigure(index=0,weight=1,minsize=450)
         
         
     def set_lyrics(self,text):
         """changes text of lyrics window"""
         
-        value = self._preprocess_text(text)
+        lines:list = self._preprocess_text(text)
+        self.Lyrics.delete(0,tk.END)
+        
+        #max_line_length = len(max(lines, key=lambda x: len(x)))
+        #print("found max line length:" , max_line_length)
+        
         if self.Lyrics is not None:
-            self.Lyrics["text"] = value
+            for line in lines:
+                self.Lyrics.insert(tk.END,f" {line:20} ")
         
 
-    def _preprocess_text(self,text):
+    def _preprocess_text(self,text:str):
         """removes unwanted words from text"""
         
-        for word in BAD_WORDS:
-            if word in text.lower():
-                text = text.replace(word,"\n")
-        return text
+        #reduce size
+        if len(text.split("\n"))>100:
+            text = "\n".join(text.split("\n")[:100])
+        
+        clean_text = []
+        # check bad words
+        
+        for line in text.split("\n"):
+            for word in BAD_WORDS:
+                ind = line.lower().find(word)
+                if ind != -1:
+                    line = line.replace(line[ind:ind+len(word)],"\n",-1)
+                
+            # devide line if it is too long
+            if len(line)>30:
+                for ind, char in enumerate(line[25:35]):
+                    if char == " ":
+                        line = line.replace(line[ind],"\n")
+                        break
+                        
+            clean_text.append(line)
+        return clean_text
         
         
 
